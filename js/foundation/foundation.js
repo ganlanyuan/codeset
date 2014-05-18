6c12b5371bb94819b852d4e21a46651994761cf5
rongsheng <ganlanyuan@gmail.com>
2014-05-17 23:22:51 -0400

    update foundation & lazyload

--- js/foundation/foundation.js
+++ js/foundation/foundation.js
@@ -1,16 +1,27 @@
 // @koala-prepend "components/foundation.js"
-// @koala-prepend "components/foundation.abide.js"
-// @koala-prepend "components/foundation.accordion.js"
-// @koala-prepend "components/foundation.alert.js"
-// @koala-prepend "components/foundation.clearing.js"
-// @koala-prepend "components/foundation.dropdown.js"
-// @koala-prepend "components/foundation.equalizer.js"
 // @koala-prepend "components/foundation.interchange.js"
-// @koala-prepend "components/foundation.joyride.js"
-// @koala-prepend "components/foundation.magellan.js"
-// @koala-prepend "components/foundation.offcanvas.js"
-// @koala-prepend "components/foundation.orbit.js"
-// @koala-prepend "components/foundation.reveal.js"
 // @koala-prepend "components/foundation.tab.js"
 // @koala-prepend "components/foundation.tooltip.js"
 // @koala-prepend "components/foundation.topbar.js"
+// @koala-prepend "components/foundation.dropdown.js"
+// @koala-prepend "components/foundation.alert.js"
+// @koala-prepend "components/foundation.equalizer.js"
+// @koala-prepend "components/foundation.magellan.js"
+// @koala-prepend "components/foundation.abide.js"
+// @koala-prepend "components/foundation.accordion.js"
+// @koala-prepend "components/foundation.joyride.js"
+// @koala-prepend "components/foundation.offcanvas.js"
+// @koala-prepend "components/foundation.reveal.js"
+// @koala-prepend "components/foundation.clearing.js"
+// @koala-prepend "components/foundation.orbit.js"
+
+// magellan.js	fixed on top when touch the window top
+// abide.js"	HTML5 form validation library
+// accordion.js"	similar with tab
+// clearing.js"	Lightbox
+// equalizer.js"	make columns same height
+// interchange.js" responsive content e.g. image
+// joyride.js"	jump to next step
+// offcanvas.js"	mobile nav
+// orbit.js"	simple slideshow
+// reveal.js"	pop-up
--- js/foundation/foundation.lte8.js
+++ js/foundation/foundation.lte8.js
@@ -1,14 +1,25 @@
-// @koala-prepend "components/foundation.abide.ie7.js"
-// @koala-prepend "components/foundation.accordion.ie7.js"
+// @koala-prepend "components/foundation.interchange.lte8.js"
+// @koala-prepend "components/foundation.tab.ie7.js"
+// @koala-prepend "components/foundation.dropdown.ie7.js"
+// @koala-prepend "components/foundation.topbar.lte8.js"
 // @koala-prepend "components/foundation.alert.ie7.js"
 // @koala-prepend "components/foundation.alert.lte8.js"
-// @koala-prepend "components/foundation.clearing.ie7.js"
-// @koala-prepend "components/foundation.dropdown.ie7.js"
 // @koala-prepend "components/foundation.equalizer.lte8.js"
-// @koala-prepend "components/foundation.interchange.lte8.js"
+// @koala-prepend "components/foundation.abide.ie7.js"
+// @koala-prepend "components/foundation.accordion.ie7.js"
+// @koala-prepend "components/foundation.clearing.ie7.js"
 // @koala-prepend "components/foundation.magellan.lte8.js"
 // @koala-prepend "components/foundation.offcanvas.lte8.js"
 // @koala-prepend "components/foundation.orbit.ie7.js"
 // @koala-prepend "components/foundation.reveal.ie7.js"
-// @koala-prepend "components/foundation.tab.ie7.js"
-// @koala-prepend "components/foundation.topbar.lte8.js"
+
+// magellan.js	fixed on top when touch the window top
+// abide.js"	HTML5 form validation library
+// accordion.js"	similar with tab
+// clearing.js"	Lightbox
+// equalizer.js"	make columns same height
+// interchange.js" responsive content e.g. image
+// joyride.js"	jump to next step
+// offcanvas.js"	mobile nav
+// orbit.js"	simple slideshow
+// reveal.js"	pop-up
