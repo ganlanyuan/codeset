ff57a80ba293c541719fbb7d66644451312bf804
designer <ganlanyuan@gmail.com>
2014-05-20 09:07:37 -0400

    fix block-grid.scss to support ie8-, rm flex-video js

--- js/script.js
+++ js/script.js
@@ -12,29 +12,4 @@
 <noscript><img src="lazy.jpg" width="100" height="100"></noscript>
 */
 
-/* make youtube iframe responsible
- * change "http://www.youtube.com" to your code or remove it
- * change ".iframe-container" to your iframe container class
- */
-$(function() {
-	var $allVideos = $("iframe[src^='http://www.youtube.com']"),
-	$fluidEl = $(".iframe-container");
-	$allVideos.each(function() {
-		$(this)
-			.data('aspectRatio', this.height / this.width)
-			.removeAttr('height')
-			.removeAttr('width');
-	});
-
-	$(window).resize(function() {
-		var newWidth = $fluidEl.width();
-		$allVideos.each(function() {
-			var $el = $(this);
-			$el
-				.width(newWidth)
-				.height(newWidth * $el.data('aspectRatio'));
-		});
-	}).resize();
-});
-
 // @koala-prepend "foundation/foundation.js"
